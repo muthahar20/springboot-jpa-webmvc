b0fd5296be61f9089042bccf6c6deaa1c4396a77
muthahar20@gmail.com <muthahar20@gmail.com>
2024-01-04 02:02:22 -0500

    initial commit

---
+++ src/main/java/com/mtr/springbootjpawebmvc/SpringbootJpaWebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootjpawebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaWebmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtr/springbootjpawebmvc/SpringbootJpaWebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootjpawebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaWebmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
