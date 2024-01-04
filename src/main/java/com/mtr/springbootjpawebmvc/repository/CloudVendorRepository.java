1d4462228a838125a796ed44eda81cf7d992343e
muthahar20@gmail.com <muthahar20@gmail.com>
2024-01-04 13:47:37 -0500

    exception handled added

---
+++ src/main/java/com/mtr/springbootjpawebmvc/repository/CloudVendorRepository.java
@@ -0,0 +1,11 @@
+package com.mtr.springbootjpawebmvc.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mtr.springbootjpawebmvc.model.CloudVendor;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+    List<CloudVendor> findByVendorName(String vendorName);
+}
---
+++ src/main/java/com/mtr/springbootjpawebmvc/service/impl/CloudVendorServiceImpl.java
@@ -0,0 +1,59 @@
+package com.mtr.springbootjpawebmvc.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mtr.springbootjpawebmvc.model.CloudVendor;
+import com.mtr.springbootjpawebmvc.repository.cloudVendorRepository;
+import com.mtr.springbootjpawebmvc.service.CloudVendorService;
+
+@Service
+public class CloudVendorServiceImpl implements CloudVendorService {
+
+    CloudVendorRepository cloudVendorRepository;
+
+    public CloudVendorServiceImpl(CloudVendorRepository cloudVendorRepository) {
+        this.cloudVendorRepository = cloudVendorRepository;
+    }
+
+    @Override
+    public String createCloudVendor(CloudVendor cloudVendor) {
+        // More Business Logic
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String updateCloudVendor(CloudVendor cloudVendor) {
+        // More Business Logic
+        cloudVendorRepository.save(cloudVendor);
+        return "Success";
+    }
+
+    @Override
+    public String deleteCloudVendor(String cloudVendorId) {
+        // More Business Logic
+        cloudVendorRepository.deleteById(cloudVendorId);
+        return "Success";
+    }
+
+    @Override
+    public CloudVendor getCloudVendor(String cloudVendorId) {
+        // More Business Logic
+        if(cloudVendorRepository.findById(cloudVendorId).isEmpty())
+            throw new CloudVendorNotFoundException("Requested Cloud Vendor does not exist");
+        return cloudVendorRepository.findById(cloudVendorId).get();
+    }
+
+    @Override
+    public List<CloudVendor> getAllCloudVendors() {
+        // More Business Logic
+        return cloudVendorRepository.findAll();
+    }
+
+    @Override
+    public List<CloudVendor> getByVendorName(String vendorName)
+    {
+        return cloudVendorRepository.findByVendorName(vendorName);
+    }
